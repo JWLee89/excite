63be56039f3f87a1a78d299a2725343ddfb2361f
Jay Lee <ljay189@gmail.com>
2020-01-16 13:29:30 +0900

    Added a concurrent.py package to help with concurrent programming in the near future. Will start working on this after I get some downtime

---
+++ excite/concurrent.py
@@ -0,0 +1,16 @@
+"""
+    @Author Jay Lee
+    My attempt to provide a set of generic functions
+    that enable ease of leverage of the power that
+    concurrency brings to programs.
+    TODO: Work on this during my downtime
+"""
+
+
+def parallel_process():
+    """
+        A function that causes a feature to run in parallel.
+        Note that it is highly recommended to not add
+        :return:
+    """
+    pass
